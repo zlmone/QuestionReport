a259f23b46fb552f790a28ba5827d48e68281550
lll <1264864361@qq.com>
2020-01-07 17:27:35 +0800

    commit

---
+++ code/QuestionReport/src/main/java/com/cnedutech/controller/TestController.java
@@ -0,0 +1,28 @@
+package com.cnedutech.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.cnedutech.mapper.UserMapper;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+
+@Controller
+public class TestController {
+
+	@Autowired
+	UserMapper userMapper;
+	
+	@RequestMapping(value="test")
+	public String  test() {
+		PageHelper.startPage(1, 10);
+		List<Map<String, Object>> user = userMapper.getUser();
+		PageInfo<Map<String, Object>> pageInfo = new PageInfo<Map<String, Object>>(user);
+		System.err.println(pageInfo);
+		return "index";
+	}
+}
